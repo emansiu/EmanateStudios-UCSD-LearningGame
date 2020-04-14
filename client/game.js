d5cf73ce971d84ed4caa4a4b82cf33e5ca2df6e5
Emanuel Siu <e@emanuelsiu.com>
2020-04-14 08:15:37 -0700

    image blocker created and cube animated

--- client/game.js
+++ client/game.js
@@ -1,2 +1,32 @@
-gsap.to(".box", { delay: 1, duration: 2, x: 300 });
-gsap.to(".green", { duration: 3, rotation: 360, scale: 0.5 });
+
+
+const canvas = document.getElementById('game')
+
+const globalScale = 1;
+
+const levels = 6;
+const numberOfRounds = 30;
+
+const square = canvas.getContext("2d");
+square.beginPath();
+square.rect(20, 20, 50, 50);
+square.fillStyle = "#FF0000";
+square.fill();
+square.closePath();
+
+let demonAttributes = {
+    eyeDiameter: [15, 30], //<-- 15px goes left, 30px goes right
+    eyeColor: ['yellow', 'blue'],
+    eyeShade: [1, 2, 3, 4, 5],
+    color: ['red', 'gray'],
+    hornHeightMin: 15,
+    hornHeightMax: 60,
+    hornWidthMin: 5,
+    hornWidthMax: 20,
+    bodyHeight: 20,
+    bodyWidth: 20
+}
+
+gsap.to("#game", { delay: 1, duration: 2, x: 300, y: -400 });
+
+console.log(demonAttributes.eyeColor)
